937943fabddcbac0fc6ec10f4f4d219fbc2ca86b
Gareth <gareth.reeve50@gmail.com>
2025-03-12 15:05:57 +0200

    altered structure

--- frontend/script.js
+++ frontend/script.js
@@ -5,6 +5,7 @@ const generateButton = document.getElementById("generate-data");
 
 // add event listners
 fileInput.addEventListener("change", uploadImage);
+generateButton.addEventListener("click", generateData);
 
 dropZone.addEventListener("click", () => fileInput.click());
 
@@ -20,17 +21,23 @@ dropZone.addEventListener("dragleave", () => {
 dropZone.addEventListener("drop", (e) => {
     e.preventDefault();
     dropZone.style.backgroundColor = "#fafafa";
-    const files = e.dataTransfer.files;
+    const files = e.dataTransfer.files; // get dropped file
+    fileInput.files = files; // assign dropped file to file input
+    
     if (files.length > 0 && files[0].type.startsWith("image/")) {
-        uploadedImage = files[0];
-        let imgUrl = URL.createObjectURL(uploadedImage); // get the image url
-        dropZone.style.backgroundImage = `url(${imgUrl})`; // set the image url as background image
+        uploadImage();
     }
 });
 
+// function to upload image to dropzone
 function uploadImage() {
     const fileObj = fileInput.files[0]; // get the file object
     let imgUrl = URL.createObjectURL(fileObj); // get the image url
     dropZone.style.backgroundImage = `url(${imgUrl})`; // set the image url as background image
     
 }
+
+async function generateData() {
+    console.log(fileInput.files[0]);
+}
+
